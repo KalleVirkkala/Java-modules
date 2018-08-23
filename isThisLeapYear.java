74fe899ba5a2adbc18384e3ab9835cf2baf6b4b2
Kalle Virkkala <39480219+KalleVirkkala@users.noreply.github.com>
2018-08-23 17:58:50 +0300

    Add files via upload

---
+++ isThisLeapYear.java
@@ -0,0 +1,29 @@
+ public static boolean isThisLeapYear(int year) {
+		//calculate if leap year from year 1 to 9999
+		//method 1
+       /* if (year >= 1 && year <= 9999) {
+
+            if (year % 4 != 0) {
+                return false;
+            } else if (year % 400 == 0) {
+                return true;
+            } else if (year % 100 == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }else {
+
+            return false;
+        }*/
+		//method 2
+		
+        if (year >= 1 && year <= 9999) {
+            return java.time.Year.of(year).isLeap();
+        }else {
+
+            return false;
+        }
+
+
+    }
